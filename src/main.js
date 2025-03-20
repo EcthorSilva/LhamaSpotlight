6f87bf381337294e4bdaff6e4db48de4687bb82f
Ecthor Silva <EcthorSilva@outlook.com>
2025-03-20 15:04:48 -0300

    change: alterado o icone do tray e adicionado um icone para a dock

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 const { app, globalShortcut } = require('electron');
 const createMainWindow = require('./windows/mainWindow');
 const createTray = require('./windows/tray');
+const path = require('path');
 
 let mainWindow;
 let tray;
@@ -8,6 +9,8 @@ let tray;
 app.whenReady().then(() => {
     mainWindow = createMainWindow();
     tray = createTray(mainWindow);
+    
+    app.dock.setIcon(path.join(__dirname, './assets/lama-2.png'));
 
     globalShortcut.register('Shift+Command+Space', () => {
         if (mainWindow.isVisible()) {
--- src/windows/tray.js
+++ src/windows/tray.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { dialog } = require('electron');
 
 function createTray(mainWindow) {
-    const trayIcon = nativeImage.createFromPath(path.join(__dirname, '../assets/llama.png')).resize({ width: 20, height: 20 });
+    const trayIcon = nativeImage.createFromPath(path.join(__dirname, '../assets/lama.png')).resize({ width: 20, height: 20 });
     let tray = new Tray(trayIcon);
 
     const trayMenu = Menu.buildFromTemplate([
